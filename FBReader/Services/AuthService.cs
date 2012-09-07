c953c6536e936a15787a4b5d37c8b9cf19310c9d
Dushyant Bansal <dushyant37@gmail.com>
2012-09-07 16:42:17 -0700

    auth service basic file.

---
+++ FBReader/Services/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBReader.Services
+{
+    public class AuthService
+    {
+        public AuthService()
+        {
+
+        }
+
+        public string FetchAuthToken()
+        {
+            return null;
+        }
+    }
+}
