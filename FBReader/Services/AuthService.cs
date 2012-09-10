bfb4878b6a751cc4a063bf309cd558fe55f3d68b
Dushyant Bansal <dushyant37@gmail.com>
2012-09-10 04:03:51 -0700

    Return access token as a task from auth service.

--- FBReader/Services/AuthService.cs
+++ FBReader/Services/AuthService.cs
@@ -11,17 +11,13 @@ namespace FBReader.Services
     {
         private String FacebookClientID = "328608250568415";
         private String FacebookCallbackUrl = "https://www.facebook.com/connect/login_success.html";
-        public String access_token;
+
         public AuthService()
         {
-            FetchAuthToken();
         }
 
-
-
-        private async void FetchAuthToken()
+        public async Task<string> FetchAuthToken()
         {
-
             try
             {
                 String FacebookURL = "https://www.facebook.com/dialog/oauth?client_id=" + Uri.EscapeDataString(FacebookClientID) +
@@ -33,23 +29,27 @@ namespace FBReader.Services
                 if (webAuthenticationResult.ResponseStatus == WebAuthenticationStatus.Success)
                 {
                     System.Diagnostics.Debug.WriteLine("Authentication Success");
-                    access_token = webAuthenticationResult.ResponseData.ToString().Substring(webAuthenticationResult.ResponseData.ToString().IndexOf('#') + 1);
+                    string access_token = webAuthenticationResult.ResponseData.ToString().Substring(webAuthenticationResult.ResponseData.ToString().IndexOf('#') + 1);
                     System.Diagnostics.Debug.WriteLine("\nAcess Token is =" + access_token);
+                    return access_token;
 
                 }
                 else if (webAuthenticationResult.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
                 {
                     System.Diagnostics.Debug.WriteLine("HTTP Error returned by AuthenticateAsync() : " + webAuthenticationResult.ResponseErrorDetail.ToString());
+                    return null;
                 }
                 else
                 {
                     System.Diagnostics.Debug.WriteLine("Error returned by AuthenticateAsync() : " + webAuthenticationResult.ResponseStatus.ToString());
+                    return null;
 
                 }
             }
             catch (Exception Error)
             {
                 System.Diagnostics.Debug.WriteLine(Error.ToString());
+                return null;
             }
         }
     }
