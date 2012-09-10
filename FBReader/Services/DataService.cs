37d66d0b56b0f72754cd75614720f7e2de37dc11
Dushyant Bansal <dushyant37@gmail.com>
2012-09-10 03:17:22 -0700

    Changes to reflect change in json response data.

--- FBReader/Services/DataService.cs
+++ FBReader/Services/DataService.cs
@@ -17,28 +17,33 @@ namespace FBReader.Services
     {
         public string id;
         public string name;
+        public string gender;
+        public string relationship_status;
+
     }
 
     public class FBProfile : FBMiniProfile
     {
+        public class FriendsListContainer
+        {
+            public List<FBMiniProfile> data;
+        }
         public string first_name;
         public string last_name;
         public string link;
         public string username;
-        public string gender;
         public string locale;
-        public string relationship_status;
+        public FriendsListContainer friends;
     }
 
-    public class FriendsListContainer
-    {
-        public List<FBMiniProfile> data;
-    }
+    
 
     public class FBData
     {
         private const string _baseurl = "https://graph.facebook.com/";
+
         private HttpClient httpClient;
+        private AuthService authService;
 
         private ObservableCollection<FBMiniProfile> _FBItems = new ObservableCollection<FBMiniProfile>();
         public ObservableCollection<FBMiniProfile> FBItems
@@ -49,12 +54,19 @@ namespace FBReader.Services
 
         public FBData()
         {
+            this.authService = new AuthService();
             this.httpClient = getHttpClient();
             //this.FetchUserProfile("btaylor");
-            string access_token = "AAAAAAITEghMBANKaSgSRJ6d0VP6LWBx8Ddbc2lPJlyewCmx4F1TZAG4ZB3xfZA8iZCLvwt6dB2d8TI3EiNP7POhD6SZCZAwABXT844til7UwZDZD";
+            //string access_token = "AAAAAAITEghMBANKaSgSRJ6d0VP6LWBx8Ddbc2lPJlyewCmx4F1TZAG4ZB3xfZA8iZCLvwt6dB2d8TI3EiNP7POhD6SZCZAwABXT844til7UwZDZD";
             //this.FetchUserFriends("me", access_token);
-            this.GetRStatusSingleFriends("me", access_token);
+            this.test();
         }
+
+        private async void test()
+        {
+            List<FBMiniProfile> friends = await this.GetRStatusSingleFriends("me", authService.access_token);
+        }
+
 
         private static HttpClient getHttpClient()
         {
@@ -75,22 +87,19 @@ namespace FBReader.Services
             return returnObject;
         }
 
-        public async Task<FBProfile> FetchUserProfile(string username, string auth_token)
+        private string constructProfileUrl(string username, string access_token)
+        {
+            return _baseurl + username + "?access_token=" + access_token + "&expires_in=5184000&fields=id,name,relationship_status,friends.fields(relationship_status,gender,name)";
+        }
+
+        public async Task<FBProfile> FetchUserProfile(string username, string access_token)
         {
             try
             {
 
-                string url = _baseurl + username + "?access_token=" + auth_token;
+                string url = constructProfileUrl(username, access_token);
                 var jsonResponse = await httpClient.GetByteArrayAsync(url);
                 FBProfile profile = (FBProfile)parseJson(jsonResponse, typeof(FBProfile));
-
-                //HttpResponseMessage response = await httpClient.GetAsync(_url);
-                //response.EnsureSuccessStatusCode();
-
-                //string statusText = response.StatusCode + " " + response.ReasonPhrase + Environment.NewLine;
-                //string responseBodyAsText = await response.Content.ReadAsStringAsync();
-                //responseBodyAsText = responseBodyAsText.Replace("<br>", Environment.NewLine); // Insert new lines
-                //string outputView = responseBodyAsText;
                 Debug.WriteLine("response {0}", profile.name);
                 return profile;
 
@@ -102,7 +111,7 @@ namespace FBReader.Services
             }
         }
 
-        private bool IsGirlWithRStatusSingle(FBProfile profile)
+        private bool IsGirlWithRStatusSingle(FBMiniProfile profile)
         {
             if (profile.gender.Equals("male") || profile.relationship_status == null)
                 return false;
@@ -114,47 +123,20 @@ namespace FBReader.Services
            
         }
 
-
-
-        private async Task<List<FBMiniProfile>> FetchUserFriends(string username, string auth_token)
+        public async Task<List<FBMiniProfile>> GetRStatusSingleFriends(string username, string access_token)
         {
-            try
+            
+            FBProfile userProfile = await FetchUserProfile(username, access_token);
+
+            List <FBMiniProfile> resultProfiles = new List<FBMiniProfile>();
+
+            foreach (var profile in userProfile.friends.data)
             {
-                string url = _baseurl+ username + "/friends?access_token=" + auth_token;
-
-                var jsonResponse = await httpClient.GetByteArrayAsync(url);
-                FriendsListContainer friendListContainer = (FriendsListContainer)parseJson(jsonResponse, typeof(FriendsListContainer));
-
-                //MemoryStream stream = new MemoryStream(jsonResponse);
-                //DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(FriendsListContainer));
-
-                //friends = (List<FBMiniProfile>)ser.ReadObject(stream);
-                //FriendsListContainer test = (FriendsListContainer)ser.ReadObject(stream);
-
-                Debug.WriteLine("friends {0}", friendListContainer.data);
-                return friendListContainer.data;
-            }
-            catch (HttpRequestException hre)
-            {
-                Debug.WriteLine("http exception {0}", hre.ToString());
-                return null;
-            }
-
-        }
-
-        public async Task<List<FBProfile>> GetRStatusSingleFriends(string username, string auth_token)
-        {
-            List<FBProfile> singleProfiles = new List<FBProfile>();
-            List<FBMiniProfile> friendsList = await FetchUserFriends(username, auth_token);
-
-            foreach (var friend in friendsList)
-            {
-                FBProfile profile = await FetchUserProfile(friend.id, auth_token);
                 if (IsGirlWithRStatusSingle(profile))
-                    singleProfiles.Add(profile);
+                    resultProfiles.Add(profile);
             }
 
-            return singleProfiles;
+            return resultProfiles;
         }
 
 
