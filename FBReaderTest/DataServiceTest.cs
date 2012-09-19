e6e0df2513e1fa25e083273f37bbfe2eb2f85d84
Dushyant Bansal <dushyant37@gmail.com>
2012-09-19 13:31:04 +0530

    Initial commit to get started with unit testing.

---
+++ FBReaderTest/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FBReader;
+
+namespace FBReaderTest
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
