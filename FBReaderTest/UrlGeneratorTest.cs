22d44de616c83515b1597781dfcb52d52786a74f
Dushyant Bansal <dushyant37@gmail.com>
2012-09-19 14:29:53 +0530

    Start writing test for url generator.

---
+++ FBReaderTest/UrlGeneratorTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FBReader;
+using FBReader.Services;
+
+namespace FBReaderTest
+{
+    [TestClass]
+    public class UrlGeneratorTest
+    {
+        [TestMethod]
+        public void ProfileUrlValid()
+        {
+            UrlGenerator urlGenerator = new UrlGenerator();
+            string username = "dushyant";
+            string access_token = "access_token=1234";
+            string resultProfileUrl = "https://graph.facebook.com/dushyant?access_token=1234&fields=id,name,relationship_status,friends.fields(relationship_status,gender,name)";
+            Assert.AreEqual(urlGenerator.constructProfileUrl(username, access_token) , resultProfileUrl);
+        }
+
+    }
+}
